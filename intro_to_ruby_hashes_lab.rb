ef135530c96c3c7bbc75b6f95cd2bea2d0de7b80
Bryna Dunnells <badunnells@gmail.com>
2019-05-06 16:54:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,21 +10,8 @@ end
 
 def monopoly
 	monopoly = {
-		:railroads => {
-
-		},
-		:names => {
-			:reading_railroad => {},
-			:pennsylvania_railroad => {},
-			:b_and_o_railroad => {},
-			:shortline => {}
-		},
-		:rent_in_dollars => {
-			:one_piece_owned => 25,
-			:two_pieces_owned => 50,
-			:three_pieces_owned => 100,
-			:four_pieces_owned => 200
-		}
+		:railroads => {},
+		
 	}
 
 end
